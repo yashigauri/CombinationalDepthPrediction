b3ef600583b0dd8313e99bef6975386c38f70a75
yashigauri <yashi.1gauri@gmail.com>
2025-02-28 10:56:19 +0530

    Added predict.py for making predictions

---
+++ predict.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import joblib
+model = joblib.load("combinational_depth_model.pkl")
+#One can edit datas here to predict
+sample_data = {
+    "Num Gates": [120],
+    "Fan-In": [6],
+    "Fan-Out": [5],
+    "Logic Levels": [9]
+}
+input_df = pd.DataFrame(sample_data)
+prediction = model.predict(input_df)
+print(f"Predicted Combinational Depth: {prediction[0]}")
